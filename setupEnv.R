374ba49c2cc7c4dd9941a892fb4a96452ee2d413
Christian Mertes <mertes@in.tum.de>
2020-04-12 21:14:54 +0200

    use binary and source if available

--- setupEnv.R
+++ setupEnv.R
@@ -4,7 +4,8 @@ START_TIME <- Sys.time()
 
 print_log <- function(...){
     hash_line <- paste0(rep("#", 10), collapse="")
-    message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
+    message("\n", hash_line, "\n### ", date(), ": ", 
+            ..., "\n", hash_line, "\n")
 }
 
 installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
@@ -25,19 +26,22 @@ INSTALL <- BiocManager::install
 # because of https://github.com/r-windows/rtools-installer/issues/3
 if("windows" == .Platform$OS.type){
     print_log("Install XML on windows ...")
-    BTYPE <- "win.binary"
-    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
+    BTYPE <- "both"
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", 
+            "AnnotationDbi", "BiocCheck"))
     
     print_log("Install source packages only for windows ...")
-    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
+    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", 
+            "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
 } else {
     BTYPE <- "source"
 }
 
 # install needed packages
-# add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
-for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils", 
-            "GenomeInfoDbData", "rtracklayer", "hms")){
+# add testthat to pre installation dependencies due to: 
+# https://github.com/r-lib/pkgload/issues/89
+for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", 
+            "BiocCheck", "R.utils", "GenomeInfoDbData", "rtracklayer", "hms")){
     installIfReq(p=p, type=BTYPE, Ncpus=NCPUS)
 }
 
