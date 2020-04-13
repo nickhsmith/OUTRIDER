5f04d47cc3e6191680eea6ed5d95c509d9bfa259
Christian Mertes <mertes@in.tum.de>
2020-04-13 20:32:01 +0200

    add cache again

--- setupEnv.R
+++ setupEnv.R
@@ -9,12 +9,9 @@ print_log <- function(...){
 }
 
 installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
-    for(j in p){
-        if(!requireNamespace(j, quietly=TRUE)){
-            print_log("Install ", j)
-            INSTALL(j, type=type, Ncpus=Ncpus, ...)
-        }
-    }
+    p <- p[!p %in% installed.packages()[,"Package"]]
+    print_log("Install ", paste(p, collapse=", "))
+    INSTALL(p, type=type, Ncpus=Ncpus, ...)
 }
 
 # install Bioconductor dependent on the R version
@@ -27,12 +24,9 @@ INSTALL <- BiocManager::install
 if("windows" == .Platform$OS.type){
     print_log("Install XML on windows ...")
     BTYPE <- "both"
-    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", 
-            "AnnotationDbi", "BiocCheck"))
-    
-    print_log("Install source packages only for windows ...")
-    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", 
-            "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi",
+            "BiocCheck", "GenomeInfoDbData", "org.Hs.eg.db", 
+            "TxDb.Hsapiens.UCSC.hg19.knownGene"))
 } else {
     BTYPE <- "source"
 }
@@ -40,10 +34,9 @@ if("windows" == .Platform$OS.type){
 # install needed packages
 # add testthat to pre installation dependencies due to: 
 # https://github.com/r-lib/pkgload/issues/89
-for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", 
-            "BiocCheck", "R.utils", "GenomeInfoDbData", "rtracklayer", "hms")){
-    installIfReq(p=p, type=BTYPE, Ncpus=NCPUS)
-}
+installIfReq("testthat")
+installIfReq(p=c("XML", "xml2", "devtools", "covr", "roxygen2", 
+        "BiocCheck", "R.utils", "GenomeInfoDbData", "rtracklayer", "hms"))
 
 # install OUTRIDER with its dependencies with a timeout due to 
 # travis (50 min) and appveyor (60 min) set installation warmup to 30 min max
